0e3bc035c50300c119cbd35be947cbd0d48b641a
Subcom5 <141753967+Subcom5@users.noreply.github.com>
2025-06-07 11:45:45 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -52,3 +52,47 @@ class TestCourses:
             max_score="100",
             min_score="10"
         )
+
+    def test_edit_course(
+            self,
+            create_course_page: CreateCoursePage,
+            courses_list_page: CoursesListPage
+    ):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+        create_course_page.image_upload_widget.upload_preview_image(
+            './testdata/files/image.png', 'create-course-preview'
+        )
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            estimated_time="2 weeks",
+            max_score="100",
+            min_score="10"
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(
+            title="Test",
+            estimated_time="Test",
+            description="Test",
+            max_score="10",
+            min_score="1"
+        )
+        create_course_page.course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Test",
+            estimated_time="Test",
+            max_score="10",
+            min_score="1"
+        )
